2f60ee94fdec1e5f45eeb66b225049a7303d0fc3
Colin Arel <romans_one16@live.com>
2020-09-02 15:25:11 -0500

    completed Filtering Criminals by Crime

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -5,7 +5,27 @@
 import { useConvictions } from "./ConvictionProvider.js"
 
 // Get a reference to the DOM element where the <select> will be rendered
+const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".filters__crime")
+
+// On the event hub, listen for a "change" event.
+eventHub.addEventListener("change", event => {
+
+    // Only do this if the `crimeSelect` element was changed
+    if (event.target.id === "crimeSelect") {
+        // Create custom event. Provide an appropriate name.
+        const customEvent = new CustomEvent("crimeChosen", {
+            detail: {
+                crimeThatWasChosen: event.target.value
+            }
+        })
+
+        // Dispatch to event hub
+        eventHub.dispatchEvent(customEvent)
+    }
+})
+
+// EXPORT FUNCTION***********************
 
 export const ConvictionSelect = () => {
     // Get all convictions from application state
@@ -17,6 +37,8 @@ export const ConvictionSelect = () => {
     sortedArray.sort();
     render(sortedArray)
 }
+
+//RENDER*************************************
 
 const render = (convictionsCollection) => {
     /*
